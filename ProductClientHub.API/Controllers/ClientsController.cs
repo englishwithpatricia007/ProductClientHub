51e41f9dea97a049ead8fb9096083cb0643cbfa3
Patricia <englishwithpatricia007@gmail.com>
2025-05-23 21:48:39 -0300

    Add project files.

---
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.API.UseCases.Clients.Register;
+using ProductClientHub.Communication.Requests;
+using ProductClientHub.Communication.Responses;
+
+namespace ProductClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] RequestClientJson request)
+        {
+            var useCase = new RegisterClientUseCase();
+            var response = useCase.Execute(request);
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id)
+        {
+            return Ok("Client updated successfully.");
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("All clients retrieved successfully.");
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok("All clients retrieved successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            return Ok("Client deleted successfully.");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ProductClientHub.API/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(c => c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0);
+    app.UseSwaggerUI(); 
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
