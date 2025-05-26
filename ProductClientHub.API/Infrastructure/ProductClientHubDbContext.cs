af1a09a90f23cd49e667d61102eb98ef92e92692
Patricia <englishwithpatricia007@gmail.com>
2025-05-26 17:40:21 -0300

    Configurando o entity framework

---
+++ ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\glitc\\Documents\\NET\\1737062251373-attachment.octet-stream");
+        }
+    }
+}
